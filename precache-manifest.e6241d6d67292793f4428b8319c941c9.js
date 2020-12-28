396a88d0e0d92a9af679050bae6d8463c472ba54
manish.nagaraj12@gmail.com <manish.nagaraj12@gmail.com>
2020-12-28 20:36:56 +0530

    Updates

---
+++ precache-manifest.e6241d6d67292793f4428b8319c941c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "588b7925a513d3ed3aeab95eee6fee90",
+    "url": "/manish-ai/Portfolio.git/index.html"
+  },
+  {
+    "revision": "78ec03f4fd5f79f38b06",
+    "url": "/manish-ai/Portfolio.git/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/manish-ai/Portfolio.git/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/manish-ai/Portfolio.git/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78ec03f4fd5f79f38b06",
+    "url": "/manish-ai/Portfolio.git/static/js/main.1bf70c72.chunk.js"
+  },
+  {
+    "revision": "32970cf188a07e6bbc07",
+    "url": "/manish-ai/Portfolio.git/static/js/runtime-main.fb7c27f2.js"
+  }
+]);
